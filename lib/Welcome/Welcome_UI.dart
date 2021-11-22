4bec25dc74cdd1a037c95db4b0be71addf18e4c1
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2021-11-23 00:18:45 +0700

    add color

--- lib/Welcome/Welcome_UI.dart
+++ lib/Welcome/Welcome_UI.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ministop/components/rounded_button.dart';
+
+import '../constants.dart';
 
 class Welcome_UI extends StatefulWidget {
   const Welcome_UI({Key? key}) : super(key: key);
@@ -15,7 +18,7 @@ class _Welcome_UIState extends State<Welcome_UI> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      backgroundColor: yellow_ministop,
       body: Center(
         child: SingleChildScrollView(
           child: Column(
@@ -23,13 +26,59 @@ class _Welcome_UIState extends State<Welcome_UI> {
             children: [
               Text(
                 "WELCOME TO MINISTOP",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.blue),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: blue_ministopColor),
 
               ),
               Image.asset("assets/images/logo.png",
                 width: size.width * 0.60,
 
-              )
+              ),
+
+              SizedBox(width: 10, height: 20,),
+              TextButton(
+                  child: Text(
+                      "Đăng nhập".toUpperCase(),
+                      style: TextStyle(fontSize: 20)
+                  ),
+                style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(blue_ministopColor),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(19.0),
+                            side: BorderSide(color: blue_ministopColor)
+                        )
+                    )
+                ),
+                onPressed:() => null,
+              ),
+              SizedBox(width: 10, height: 10,),
+              TextButton(
+
+                child: Text(
+                    "Đăng ký".toUpperCase(),
+                    style: TextStyle(fontSize: 20)
+                ),
+                style: ButtonStyle(
+                    
+                    padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(blue_ministopColor),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(19.0),
+                            side: BorderSide(color: blue_ministopColor)
+                        )
+                    )
+                ),
+                onPressed:() => null,
+              ),
+
+
+
+
+
 
             ],
           ),
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
+const blue_ministopColor = Color(0xFF002672);
+const yellow_ministop = Color(0xFFFDCC32);
